13a41591228c6373f10c9c300968b94f3c683374
Alexander George Squire <alecsquire@gmail.com>
2023-12-05 08:57:47 +0000

    morning of the 5th

---
+++ snippets/test.js
@@ -0,0 +1,32 @@
+function sum(a, b) {
+    return a + b
+}
+const sum(a, b) => {return a,b}
+
+const sum = (a, b) => a + b;
+
+
+
+function isPositive(number) {
+    return number >= 0
+}
+
+cosnt isPositive = (number) => number number >= 0
+
+const 
+
+const greeting = (name) => {console.log (`hey baby girl i like your face my name is ${name}`)}
+
+console.log (Alexander George Squire)
+
+function randomNumber() {
+    return Math.random
+}
+
+let randomNumber = () => {return Math.random}
+
+
+document.addEventListener('click', function () {
+console.log ('click')
+})
+
